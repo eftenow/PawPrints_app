0bed34f0559ceb654fdb55413d2c9c410fa93556
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-07-09 20:02:49 +0300

    added search bar functionality

--- paw_prints_app/pets/forms.py
+++ paw_prints_app/pets/forms.py
@@ -6,5 +6,11 @@ from paw_prints_app.pets.models import Pet
 class PetCreateForm(forms.ModelForm):
     class Meta:
         model = Pet
-        fields = ['name', 'pet_category', 'breed', 'age', 'image', 'description' ]
+        fields = ['name', 'pet_category', 'breed', 'age', 'image', 'description']
 
+
+class SearchForm(forms.Form):
+    search = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'id': 'breedFilter', 'placeholder': "Search by breed or name.."}))
--- paw_prints_app/pets/views.py
+++ paw_prints_app/pets/views.py
@@ -1,9 +1,10 @@
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
-from paw_prints_app.pets.forms import PetCreateForm
+from paw_prints_app.pets.forms import PetCreateForm, SearchForm
 from paw_prints_app.pets.models import Pet, Breed
 
 
@@ -11,6 +12,28 @@ from paw_prints_app.pets.models import Pet, Breed
 class PetListView(ListView):
     template_name = 'pet/all-pets.html'
     model = Pet
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_form = SearchForm(self.request.GET)
+
+        if search_form.is_valid():
+            search_text = search_form.cleaned_data['search']
+
+            if search_text:
+                queryset = queryset.filter(
+                    Q(breed__name__icontains=search_text) |
+                    Q(name__icontains=search_text)
+                )
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        search_form = SearchForm(self.request.GET)
+        context['form'] = search_form
+        return context
+
 
 
 class PetCreateView(CreateView):
@@ -40,4 +63,3 @@ def get_breeds(request):
     pet_category = request.GET.get('pet_category')
     breeds = Breed.objects.filter(pet_category=pet_category).values('id', 'name').distinct('name')
     return JsonResponse({'breeds': list(breeds)})
-
