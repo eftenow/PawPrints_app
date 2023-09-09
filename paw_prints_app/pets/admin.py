2d2a6f41acce1b0120772ab0e68acb9a846a9599
eftenow <mass4cre@icloud.com>
2023-09-09 09:18:12 +0300

    added pets and breeds to the admin pannel

--- paw_prints_app/pets/admin.py
+++ paw_prints_app/pets/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pet, Breed
 
-# Register your models here.
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pet_category', 'age', 'breed', 'adoption_status', 'added_by', 'adopted_by', 'added_at']
+    search_fields = ['name', 'breed__name', 'added_by__username', 'adopted_by__username']
+    list_filter = ['pet_category', 'adoption_status', 'breed']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pet_category']
+    search_fields = ['name']
+    list_filter = ['pet_category']
